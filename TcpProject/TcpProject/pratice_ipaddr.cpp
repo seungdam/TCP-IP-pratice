5d14f0d5aeeb15a47513e20ffb98f2964b5ee229
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-07-11 23:07:14 +0900

    [TCP/ IP 소켓 프로그래밍  8-3-3] *바이트 정렬 실습 inet_ntoa() 을 사용해 네트워크 바이트 정렬된 주소를 문자열 주소로 출력

--- TcpProject/TcpProject/pratice_ipaddr.cpp
+++ TcpProject/TcpProject/pratice_ipaddr.cpp
@@ -12,11 +12,18 @@ int main() {
 	if (WSAStartup(MAKEWORD(3, 2), &wsa) != 0)
 		return 1;
 	
+	// ipv4 주소 변환
 	char* ipv4 = "127.46.114.70";
 
-	
 	cout << "[문자열 주소]" << ipv4 << endl;
+	// inet_addr 함수를 사용해 문자열 주소를 네트워크 바이트 정렬
 	cout << "[네트워크 바이트 정렬]" << inet_addr(ipv4) << endl;
+	
+	// 바이트 정렬된 주소
+	IN_ADDR ipv4num; // ipv4 주소 저장을 위한 구조체
+	ipv4num.s_addr = inet_addr(ipv4);
+
+	cout << "[바이트 정렬] -> [문자열]" << inet_ntoa(ipv4num) << endl;
 
 	WSACleanup();
 }
