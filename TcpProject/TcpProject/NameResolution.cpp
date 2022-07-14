d60dfd75591b63f673676d9972fe4d70f967b18d
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-07-14 22:49:30 +0900

    [TCP/ IP 소켓 프로그래밍  8-4-4] *도메인 실습 ipv4 주소에서 도메인 이름으로 변환하는 getDomainName() 함수를 gethostbyaddr()을 사용해 작성

--- TcpProject/TcpProject/NameResolution.cpp
+++ TcpProject/TcpProject/NameResolution.cpp
@@ -1,4 +1,5 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS // 최신 VC++ 컴파일 시 경고 방지
+#define _CRT_SECURE_NO_WARNINGS
 #pragma comment(lib, "ws2_32")
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -30,6 +31,20 @@ bool getIpAddr(char* domain_name, IN_ADDR* addr) {
 	if (ptr->h_addrtype != AF_INET)
 		return false;
 	memcpy(addr, ptr->h_addr_list, ptr->h_length);
+	return true;
+}
+
+// ip주소 -> 도메인 이름
+
+bool getDomainName(IN_ADDR addr, char* name, int namelen) {
+	HOSTENT* ptr = gethostbyaddr((char*)&addr,sizeof(addr),AF_INET);
+	if (ptr == NULL) {
+		err_display("gethostbyaddr()");
+		return false;
+	}
+	if (ptr->h_addrtype != AF_INET)
+		return false;
+	strncpy(name, ptr->h_name, namelen);
 	return true;
 }
 
