d5a1284211681584f07f6bdb7db9344c89ac3843
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-07-22 13:36:53 +0900

    [TCP/ IP 소켓 프로그래밍  12] IPV6의 TCP 서버 오류 해결 (listen()으로 tcp 상태 변경)

---
+++ TcpProject/client/client.cpp
@@ -0,0 +1,41 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // 최신 VC++ 컴파일 시 경고 방지
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+
+#define SERVERPORT 9000
+#define BUFSIZE 512
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+void err_quit(char* msg) {
+	LPVOID lpMsgBuf;
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+void err_display(char* msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	cout << "[" << msg << "] : " << (char*)lpMsgBuf << endl;
+	LocalFree(lpMsgBuf);
+}
+
+int main(int argc, char* argv) {
+
+}
