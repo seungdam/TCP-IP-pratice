2c64278d0f69949fa041f710c2cfeb9b63adfc79
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-07-28 15:52:00 +0900

    [TCP/ IP 소켓 프로그래밍 25 - 3]  *스레드 실습  resumethread()를 수행하고 waitforsingleobject()를 통해 스레드 종료까지 기다리고 결과를 출력.

--- TcpProject/tread/ExThread3.cpp
+++ TcpProject/tread/ExThread3.cpp
@@ -7,7 +7,7 @@ int retval = 0;
 unsigned __stdcall MyThread(LPVOID arg) {
 	int num = (int)arg;
 	
-	for (int i = 0; i < num; ++i) {
+	for (int i = 0; i <= num; ++i) {
 		retval += i;
 	}
 	return 0;
@@ -18,5 +18,12 @@ int main() {
 	int arg = 1000;
 	HANDLE m_thread = (HANDLE)_beginthreadex(NULL, 0, MyThread, (LPVOID)arg, CREATE_SUSPENDED, NULL);
 	if (m_thread == NULL) return 1;
-	std::cout << retval;
+	std::cout << "실행 전:" << retval << std::endl;
+
+	// 서스펜드 상태로 생성했으니 바로 수행하지 않음. -> resumethread 호출해야함.
+	ResumeThread(m_thread);
+	// 스레드가 기능을 모두 수행할 때 까지 기다림.
+	WaitForSingleObject(m_thread, INFINITE);
+	std::cout << "실행 후: " << retval << std::endl;
+	CloseHandle(m_thread);
 }
