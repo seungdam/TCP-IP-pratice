a1d6c3d570f86a7f7699ecb30104cc793810a9e4
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-07-28 21:43:54 +0900

    [TCP/ IP 소켓 프로그래밍 26 - 3] *스레드 동기화 - 임계 영역 실습 임계 영역을 사용해 버그를 픽스.

--- TcpProject/tread/ExCriticalSection.cpp
+++ TcpProject/tread/ExCriticalSection.cpp
@@ -1,32 +1,44 @@
 #include <process.h>
 #include <windows.h>
 #include <iostream>
-#define MAXNUM 100000
+#define MAXNUM 200000
 
 using namespace std;
 int num = 0;
 
+CRITICAL_SECTION cs; // 임계 영역 구조체 변수 cs 생성
+
 unsigned __stdcall MyThread(LPVOID arg) {
-	for (int i = 0; i < MAXNUM; ++i)
+	
+	for (int i = 0; i < MAXNUM; ++i) {
+		EnterCriticalSection(&cs);
 		++num;
+		LeaveCriticalSection(&cs);
+	}
 	return 0;
 }
 
 unsigned __stdcall MyThread2(LPVOID arg) {
-	for (int i = 0; i < MAXNUM; ++i)
+	for (int i = 0; i < MAXNUM; ++i) {
+		EnterCriticalSection(&cs);
 		--num;
+		LeaveCriticalSection(&cs);
+	}
 	return 0;
 }
 
 int main() {
-	
+	// 임계 영역 초기화
+	InitializeCriticalSection(&cs);
+
 	HANDLE m_thread[2];
 	m_thread[0] = (HANDLE)_beginthreadex(NULL, 0, MyThread, NULL, 0, NULL);
 	m_thread[1] = (HANDLE)_beginthreadex(NULL, 0, MyThread2, NULL, 0, NULL);
 
 	WaitForMultipleObjects(2, m_thread, TRUE, INFINITE);
 	cout << num << endl;
-	// 공유 자원을 서로다른 스레드에서 동시에 접근하기에 부적절한 출력값이 나오게 된다.
-
+	
+	// 임계 영역 삭제
+	DeleteCriticalSection(&cs);
 }
 
