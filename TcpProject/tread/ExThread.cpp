7bf67a8a117bdac8d37e708210b043ee20651957
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-07-28 14:59:29 +0900

    [TCP/ IP 소켓 프로그래밍 23 - 2]  *스레드 실습 자신의 스레드 id를 출력하는 3개의 스레드를 생성

--- TcpProject/tread/ExThread.cpp
+++ TcpProject/tread/ExThread.cpp
@@ -1,7 +1,29 @@
 #include <windows.h>
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
+DWORD WINAPI f(LPVOID arg) {
+	int a = (int)arg;
+	while(true) {
+		cout << "Thread ID: " << GetCurrentThreadId()<< endl;
+		Sleep(1500);
+	}
+}
 
 int main() {
+	HANDLE m_thread1 = CreateThread(NULL, 0, f, NULL, 0, NULL);
 
+	if (m_thread1 == NULL) return 1;
+	CloseHandle(m_thread1);
+
+	HANDLE m_thread2 = CreateThread(NULL, 0, f, NULL, 0, NULL);
+	if (m_thread2 == NULL) return 1;
+	CloseHandle(m_thread2); // 스레드를 조작할 일이 없으면 가능한 일찍 닫는 것이 중요. != 스레드 종료
+
+	while (true) {
+		cout << "Main Thread ID: " << GetCurrentThreadId() << endl;
+		Sleep(1000);
+	}
 }
