14b9db1d3a835dc365320ae2928f3fb865b8d1e0
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-07-28 15:30:43 +0900

    [TCP/ IP 소켓 프로그래밍] 프로젝트 파일 업데이트 & 실습 23의 _beginthreadex() 인자 수정

--- TcpProject/tread/ExThread.cpp
+++ TcpProject/tread/ExThread.cpp
@@ -26,11 +26,11 @@ int main() {
 	unsigned int arg2 = 2;
 	
 	// 1. 보안 변수 2. 초기 스택 사이즈 3. 스레드 함수 모듈 4. 생성 옵션 5. 스레드 id
-	HANDLE m_thread1 = (HANDLE)_beginthreadex(NULL, 0, f2, NULL, 0, &arg);
+	HANDLE m_thread1 = (HANDLE)_beginthreadex(NULL, 0, f2, &arg, 0, NULL);
 	if (m_thread1 == NULL) return 1;
 	CloseHandle(m_thread1);
 	//HANDLE m_thread2 = CreateThread(NULL, 0, f, NULL, 0, NULL);
-	HANDLE m_thread2 = (HANDLE)_beginthreadex(NULL, 0, f2, NULL, 0, &arg2);
+	HANDLE m_thread2 = (HANDLE)_beginthreadex(NULL, 0, f2, &arg2, 0, NULL);
 	if (m_thread2 == NULL) return 1;
 	CloseHandle(m_thread2); // 스레드를 조작할 일이 없으면 가능한 일찍 닫는 것이 중요. != 스레드 종료
 
