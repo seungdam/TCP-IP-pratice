798b43225a12bc5cfb4eb5068e2eefede1cc7596
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-08-02 16:10:09 +0900

    [TCP/ IP 소켓 프로그래밍 27 - 3] *스레드 동기화 - 이벤트 실습 버퍼에 bufsize 만큼 데이터를 쓰는 writeThread 작성

--- TcpProject/tread/ExEvent.cpp
+++ TcpProject/tread/ExEvent.cpp
@@ -3,9 +3,21 @@
 #include <iostream>
 
 #define BUFSIZE 10
-
+HANDLE m_readEvent;
+HANDLE m_writeEvent;
+int buf[BUFSIZE];
 unsigned __stdcall WriteThread(LPVOID) {
+	int retval;
+	for (int i = 0; i <= 500; ++i) {
+		retval = WaitForSingleObject(m_readEvent, INFINITE); // 파일 읽기가 모두 끝날 때까지 대기
+		if (retval != WAIT_OBJECT_0) break; //  읽기 스레드가 정상적으로 종료되지 않았다면 루프를 벗어남.
 
+		for (int k = 0; k < BUFSIZE; ++k) {
+			buf[k] = i;
+		}
+
+		SetEvent(m_writeEvent); // 쓰기 완료. 비신호 -> 신호
+	}
 }
 unsigned __stdcall ReadThread(LPVOID) {
 
@@ -13,5 +25,5 @@ unsigned __stdcall ReadThread(LPVOID) {
 
 
 int main() {
-
+	return 0;
 }
