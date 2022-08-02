6e8a03ec7322d838e23bada9e879afbc5cb72ad6
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-08-02 16:08:56 +0900

    [TCP/ IP 소켓 프로그래밍 27 -  2] *스레드 동기화 - 이벤트 실습 파일을 읽는 readThread와 쓰는 기능을 하는 writeThread를 선언.

---
+++ TcpProject/tread/ExEvent.cpp
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <process.h>
+#include <iostream>
+
+#define BUFSIZE 10
+
+unsigned __stdcall WriteThread(LPVOID) {
+
+}
+unsigned __stdcall ReadThread(LPVOID) {
+
+}
+
+
+int main() {
+
+}
