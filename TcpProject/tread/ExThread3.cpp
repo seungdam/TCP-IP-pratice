70db4bd6c51b93bf5e54cf06d80cf25d4e0533e0
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-08-04 00:29:37 +0900

    [TCP/ IP 소켓 프로그래밍 30] *스레드 연습 문제 ExThread3.cpp 예제를 수정해서 waitforsingleobject()가 timeout시 *를 출력하게함.

--- TcpProject/tread/ExThread3.cpp
+++ TcpProject/tread/ExThread3.cpp
@@ -2,28 +2,35 @@
 #include <windows.h>
 #include <iostream>
 
-int retval = 0;
+int sum = 0;
 
 unsigned __stdcall MyThread(LPVOID arg) {
 	int num = (int)arg;
 	
 	for (int i = 0; i <= num; ++i) {
-		retval += i;
+		sum += i;
+		Sleep(10);
 	}
 	return 0;
 }
 
 int main() {
 
+	int retval = 0;
 	int arg = 1000;
 	HANDLE m_thread = (HANDLE)_beginthreadex(NULL, 0, MyThread, (LPVOID)arg, CREATE_SUSPENDED, NULL);
 	if (m_thread == NULL) return 1;
-	std::cout << "실행 전:" << retval << std::endl;
+	std::cout << "실행 전:" << sum << std::endl;
 
 	// 서스펜드 상태로 생성했으니 바로 수행하지 않음. -> resumethread 호출해야함.
 	ResumeThread(m_thread);
 	// 스레드가 기능을 모두 수행할 때 까지 기다림.
-	WaitForSingleObject(m_thread, INFINITE);
-	std::cout << "실행 후: " << retval << std::endl;
+	retval = WaitForSingleObject(m_thread, 5000);
+	if (retval == WAIT_TIMEOUT) {
+		std::cout << "*" << std::endl;
+		WaitForSingleObject(m_thread, INFINITE);
+	}
+
+	std::cout << "실행 후: " << sum << std::endl;
 	CloseHandle(m_thread);
 }
