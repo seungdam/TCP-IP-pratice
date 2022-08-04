121e31bda0fd1d6960ae98eacf7d95256d191c42
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-08-04 23:56:52 +0900

    [TCP/ IP 소켓 프로그래밍 34] *.스레드 연습문제 ExCriticalSection.cpp의 임계영역 사용코드를 이벤트로 바꿔 작성

--- TcpProject/tread/ExCriticalSection.cpp
+++ TcpProject/tread/ExCriticalSection.cpp
@@ -8,29 +8,37 @@ int num = 0;
 
 CRITICAL_SECTION cs; // 임계 영역 구조체 변수 cs 생성
 
+HANDLE m_add_event;
+HANDLE m_sub_event;
+
 unsigned __stdcall MyThread(LPVOID arg) {
 	
 	for (int i = 0; i < MAXNUM; ++i) {
-		EnterCriticalSection(&cs);
+		//EnterCriticalSection(&cs);
+		int retval = WaitForSingleObject(m_sub_event, INFINITE);
 		++num;
-		LeaveCriticalSection(&cs);
+		//LeaveCriticalSection(&cs);
+		SetEvent(m_add_event);
 	}
 	return 0;
 }
 
 unsigned __stdcall MyThread2(LPVOID arg) {
 	for (int i = 0; i < MAXNUM; ++i) {
-		EnterCriticalSection(&cs);
+		//EnterCriticalSection(&cs);
+		int retval = WaitForSingleObject(m_add_event, INFINITE);
 		--num;
-		LeaveCriticalSection(&cs);
+		//LeaveCriticalSection(&cs);
+		SetEvent(m_sub_event);
 	}
 	return 0;
 }
 
 int main() {
 	// 임계 영역 초기화
-	InitializeCriticalSection(&cs);
-
+	//InitializeCriticalSection(&cs);
+	m_add_event = CreateEvent(NULL,FALSE,FALSE,NULL);
+	m_sub_event = CreateEvent(NULL, FALSE, TRUE, NULL);
 	HANDLE m_thread[2];
 	m_thread[0] = (HANDLE)_beginthreadex(NULL, 0, MyThread, NULL, 0, NULL);
 	m_thread[1] = (HANDLE)_beginthreadex(NULL, 0, MyThread2, NULL, 0, NULL);
@@ -39,6 +47,6 @@ int main() {
 	cout << num << endl;
 	
 	// 임계 영역 삭제
-	DeleteCriticalSection(&cs);
+	//DeleteCriticalSection(&cs);
 }
 
