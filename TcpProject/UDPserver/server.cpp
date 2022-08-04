d6702f3f3b77c9011741bd01d1ac8f4dfec7d523
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-08-05 00:05:35 +0900

    [TCP/ IP 소켓 프로그래밍 35 - 2] *UDP 실습 윈속 초기화 후 udp 소켓 sock 선언 후 포트와 ip주소를 bind함

---
+++ TcpProject/UDPserver/server.cpp
@@ -0,0 +1,59 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // 최신 VC++ 컴파일 시 경고 방지
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+
+#define SERVERPORT 9000
+#define BUFSIZE 512
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+void err_quit(char* msg) {
+	LPVOID lpMsgBuf;
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+void err_display(char* msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	cout << "[" << msg << "] : " << (char*)lpMsgBuf << endl;
+	LocalFree(lpMsgBuf);
+}
+
+int main() {
+	int retval;
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(3, 2), &wsa) != 0) return 1;
+
+	SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock != INVALID_SOCKET) err_quit("socket()");
+
+	sockaddr_in serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(SERVERPORT);
+
+	retval = bind(sock, (sockaddr*)&sock, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("bind()");
+
+
+	closesocket(sock);
+	WSACleanup();
+
+}
