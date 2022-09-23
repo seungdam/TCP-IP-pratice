9dd82795f64c9fc34db1ef1a6c27e9061f07db8b
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-09-23 22:18:34 +0900

    [TCP/ IP 소켓 프로그래밍] ** Overlapped IO 모델(I) 서버**
    - 에러 결과 출력을 위한 함수 err_quit(), err_display() 작성
    - 통신 확인용 소켓 s_sock을 overlapped io용으로 생성
    - 서버 주소 구조체를 초기화하고 s_sock에 bind()

--- TcpProject/OVERLAPPED_IO_SERVER/overlapped_io_server.cpp
+++ TcpProject/OVERLAPPED_IO_SERVER/overlapped_io_server.cpp
@@ -6,6 +6,31 @@
 #define SERVER_PORT 9000
 #define BUFSIZE 256
 
+// 오류 결과 출력을 위한 함수들
+void err_quit(char* msg) {
+	LPVOID lpMsgBuf;
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+void err_display(char* msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	std::cout << "[" << msg << "] : " << (char*)lpMsgBuf << std::endl;
+	LocalFree(lpMsgBuf);
+}
+
 SOCKET c_sock; // 클라이언트 소켓
 WSAOVERLAPPED c_over; // overlapped 구조체
 WSABUF c_wsabuf[1];
@@ -16,6 +41,20 @@ char c_msg[BUFSIZE];
 int main() {
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return -1;
+
+	SOCKET s_sock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED); // overlapped 지원하는 소켓 생성
+	if (INVALID_SOCKET == s_sock) err_quit("WSAsocket()");
+	
+	sockaddr_in serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVER_PORT);
+
+	int retval;
+	retval = bind(s_sock, (sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (SOCKET_ERROR == retval) err_quit("bind()");
+
 
 	WSACleanup();
 }
