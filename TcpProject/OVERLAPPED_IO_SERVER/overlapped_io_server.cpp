22c09b89e6e15001feaed835010851e500fa25dd
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-09-23 22:07:48 +0900

    [TCP/ IP 소켓 프로그래밍] ** Overlapped IO 모델(I) 서버**
    - 패킷 전송을 위한 구조체 변수 선언

--- TcpProject/OVERLAPPED_IO_SERVER/overlapped_io_server.cpp
+++ TcpProject/OVERLAPPED_IO_SERVER/overlapped_io_server.cpp
@@ -6,6 +6,12 @@
 #define SERVER_PORT 9000
 #define BUFSIZE 256
 
+SOCKET c_sock; // 클라이언트 소켓
+WSAOVERLAPPED c_over; // overlapped 구조체
+WSABUF c_wsabuf[1];
+char c_msg[BUFSIZE];
+
+
 // Overlapped I/O CALLBACK 모델
 int main() {
 	WSADATA wsa;
